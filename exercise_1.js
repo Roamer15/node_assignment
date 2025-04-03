463dcd2bf20aa0f5aec559e583149482d88be9dd
Beleke Ian <ianseeksbeta@gmail.com>
2025-04-03 14:06:34 +0100

    Update to simulation of exercise one

--- exercise_1.js
+++ exercise_1.js
@@ -1,18 +1,23 @@
-import {EventEmitter} from 'events'
+import { EventEmitter } from "events";
 
-const event = new EventEmitter()
-const rate = 90
+const event = new EventEmitter();
+let rate = 90;
 
 const overload = () => {
-    console.log(`Your system is overloaded at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
-}
+  console.log(
+    `Your system is overloaded at ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+  );
+};
 
-event.on('overload', overload)
+event.on("overload", overload);
+
 setInterval(() => {
-    if(rate>80){
-        event.emit('overload')
-    }
-    else
-    console.log(`System at normal rate: ${new Date()}`)
-},1000)
+    rate = Math.floor(Math.random() * 100) + 1
+    console.log(`New rate: ${rate}%`)
+},5000)
 
+setInterval(() => {
+  if (rate > 80) {
+    event.emit("overload");
+  } else console.log(`System at normal rate: ${new Date()}`);
+}, 1000);
